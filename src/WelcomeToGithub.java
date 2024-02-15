1e23503162b8d6a9baf68ffa279e2e13239d4769
uttu dacoder <lopnack@gmail.com>
2024-02-15 20:57:15 +0300

    [STUD-13] Add flow after git
    https://trello.com/c/NiFRvgCf

--- src/WelcomeToGithub.java
+++ src/WelcomeToGithub.java
@@ -2,7 +2,7 @@ public class WelcomeToGithub {
 
     public static void main(String[] args) {
         System.out.println("wazaaaa");
-        System.out.println("Hello git!");
+        System.out.println("Hello gitflow!");
         System.out.println("Oh, hell naaah");
     }
 }
