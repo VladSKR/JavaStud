f725141e464dc37285bead6c9c87d3f86ece2180
Mihail Skrebtsov <watskinov@gmail.com>
2024-03-03 20:26:29 +0300

    [STUD-16] Add CatEntity
    trello.com/c/QTAdgHtL

---
+++ src/repository/entity/CatEntity.java
@@ -0,0 +1,79 @@
+package repository.entity;
+
+import java.time.LocalDate;
+import java.util.Objects;
+import java.util.UUID;
+
+public class CatEntity extends BaseEntity {
+
+    private String breed;
+    private String color;
+
+    public CatEntity() {
+
+    }
+
+    public CatEntity(UUID id, String name, LocalDate birthDate, String breed, String color) {
+
+        super(id, name, birthDate);
+        this.breed = breed;
+        this.color = color;
+    }
+
+    public String getBreed() {
+
+        return this.breed;
+    }
+
+    public void setBreed(String breed) {
+
+        this.breed = breed;
+    }
+
+    public String getColor() {
+
+        return this.color;
+    }
+
+    public void setColor(String color) {
+
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+
+        return super.getClass().getSimpleName()
+                + "(id = " + super.getId()
+                + ", name = " + super.getName()
+                + ", birthDate = " + super.getBirthDate()
+                + ", breed = " + this.breed
+                + ", color = " + this.color + ")";
+    }
+
+    @Override
+    public int hashCode() {
+
+        return breed.length() + color.length();
+    }
+
+    @Override
+    public boolean equals(Object object) {
+
+        if (!(object instanceof CatEntity)) {
+            return false;
+        }
+
+        if (this == object) {
+            return true;
+        }
+
+        CatEntity otherCat = (CatEntity) object;
+
+        return Objects.equals(super.getId(), otherCat.getId())
+                && Objects.equals(super.getName(), otherCat.getName())
+                && Objects.equals(super.getBirthDate(), otherCat.getBirthDate())
+                && Objects.equals(this.breed, otherCat.breed)
+                && Objects.equals(this.color, otherCat.color);
+    }
+}
