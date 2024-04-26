9111daebb4bb162c7e2c0b8352567de583dae9e1
Skrebcov Mikhail <skrebcovmikhail@gmail.com>
2024-04-26 21:35:18 +0300

    [STUD-29] Make HashMap
    trello.com/c/hihb2pao/29

--- src/main/java/com/stud/java/JavaStudApplication.java
+++ src/main/java/com/stud/java/JavaStudApplication.java
@@ -1,5 +1,6 @@
 package com.stud.java;
 
+import com.stud.java.enums.Breed;
 import com.stud.java.repository.entity.CatEntity;
 import com.stud.java.repository.entity.CatEntityGenerator;
 import com.stud.java.enums.Breed;
@@ -9,6 +10,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.List;
+import java.util.Map;
 
 import java.util.List;
 import java.util.Map;
@@ -19,7 +21,9 @@ public class JavaStudApplication {
     public static void main(String[] args) {
         SpringApplication.run(JavaStudApplication.class, args);
 
-        List<CatEntity> cats = CatEntityGenerator.generateCats(1000);
+        List<CatEntity> cats = CatEntityGenerator.generateCats(100);
+        Map<Breed, Integer> breedCount = CatEntity.countCatsByBreed(cats);
+        System.out.println(breedCount);
 
         for (CatEntity cat : cats) {
             System.out.println(cat);
