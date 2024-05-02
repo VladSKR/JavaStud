d8741fd74776871ea50c6a612049c083e42abad7
Skrebcov Mikhail <skrebcovmikhail@gmail.com>
2024-05-02 10:16:18 +0300

    [STUD-29] Make a Map Method
    trello.com/c/hihb2pao

---
+++ src/main/java/com/stud/java/service/impl/CatService.java
@@ -0,0 +1,13 @@
+package com.stud.java.service.impl;
+
+import com.stud.java.enums.Breed;
+import com.stud.java.repository.entity.CatEntity;
+
+import java.util.List;
+import java.util.Map;
+
+public interface CatService {
+
+    Map<Breed, Integer> countCatsByBreed(List<CatEntity> cats);
+}
+
