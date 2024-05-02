d4fdb1e9c6eeb95f977dfdaf9b5ab2330d95dd10
Skrebcov Mikhail <skrebcovmikhail@gmail.com>
2024-05-02 10:21:26 +0300

    [STUD-30] Make a Change Color Method
    trello.com/c/9uLprudH

---
+++ src/main/java/com/stud/java/service/impl/CatService.java
@@ -0,0 +1,12 @@
+package com.stud.java.service.impl;
+
+import com.stud.java.enums.Color;
+import com.stud.java.repository.entity.CatEntity;
+
+import java.util.List;
+
+public interface CatService {
+
+    void changeColor(List<CatEntity> cats, Color newColor);
+}
+
